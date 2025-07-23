16e5045e4c48f73170275d0f65fab22a5dda4559
Gustavo Daros <72163665+guDaros@users.noreply.github.com>
2025-07-23 20:31:42 -0300

    adicionado tag google

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
-import AmandaBecineriSite from './Amanda'
+import { useEffect } from 'react';
+import AmandaBecineriSite from './Amanda';
+import TagManager from 'react-gtm-module';
 
 function App() {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'AW-16881452075' // Substitua pelo seu ID real
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []); // [] garante que execute apenas uma vez
 
   return (
-    <AmandaBecineriSite></AmandaBecineriSite>
-  )
+    <AmandaBecineriSite />
+  );
 }
 
-export default App
+export default App;
